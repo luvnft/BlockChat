389d09decba54bd8ff03624960055fb5dc0e4517
The Wizard of Hahz <info@blkluv.org>
2024-01-26 21:09:04 -0500

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { getDefaultWallets, configureChains } from 'wagmi'
+import { mainnet, goerli, sepolia } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { infuraProvider } from 'wagmi/providers/infura'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { darkTheme } from '@rainbow-me/rainbowkit/themes'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { chains, publicClient } = configureChains(
+  [mainnet, goerli, sepolia],
+  [
+    // Put your Infura API key in a `.env` file.
+    infuraProvider({
+      apiKey: process.env.INFURA_KEY ?? '',
+    }),
+    publicProvider(),
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'block chat',
+  // Put your WalletConnect Cloud project ID here.
+  projectId: 'b0a6b7ab02a4090553750be07a5728a6',
+  chains,
+})
+
+const wagmiConfig = {
+  autoConnect: true,
+  connectors,
+  publicClient,
 }
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        theme={darkTheme({
+          accentColor: '#495057',
+          accentColorForeground: 'white',
+        })}
+        chains={chains}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default MyApp
